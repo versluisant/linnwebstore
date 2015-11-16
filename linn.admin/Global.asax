c161d17d491e76935d5d0c2354b6b7416f57754e
versluisant <rackipava@gmail.com>
2015-11-17 00:28:32 +0200

    fsd
    sdf

---
+++ linn.admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="linn.admin.MvcApplication" Language="C#" %>
---
+++ linn.admin/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace linn.admin.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public Category ParentCategory { get; set; }
+    }
+}
